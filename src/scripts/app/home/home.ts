89c8c0eeca16fc8e03b159fda5765b4d16805f26
Quintonn Rothmann <quintonn.rothmann@lyniate.com>
2021-04-16 12:43:08 +0300

    wip: fixing app

--- src/scripts/app/home/home.ts
+++ src/scripts/app/home/home.ts
@@ -34,20 +34,24 @@ class HomeComponentController implements ng.IOnInit {
       window.location.href.indexOf('?code') > -1 ||
       window.location.href.indexOf('?state') > -1
     ) {
-      setTimeout(function () {
-        console.log('calling oauth.ready');
-        console.log(window.location);
+      console.log('onReady home');
+      self.$location.path('/queue');
+      self.$scope.$apply();
 
-        const onReadyCallback = function (x: any) {
-          const s = self;
-          console.log('calling self.onReady');
-          console.log(s);
-          self.onReady(s, x);
-        };
+      // setTimeout(function () {
+      //   console.log('calling oauth.ready');
+      //   console.log(window.location);
 
-        FHIR.oauth2.ready(onReadyCallback, self.onError);
-        console.log('auth ready called XXX');
-      }, 100);
+      //   const onReadyCallback = function (x: any) {
+      //     const s = self;
+      //     console.log('calling self.onReady');
+      //     console.log(s);
+      //     self.onReady(s, x);
+      //   };
+
+      //   FHIR.oauth2.ready(onReadyCallback, self.onError);
+      //   console.log('auth ready called XXX');
+      // }, 100);
     }
   }
 
